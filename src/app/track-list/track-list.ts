e48d2db067044fc2e00d1e428e08f9591a13c4e4
sanedoma <ous.ndiaye470@gmail.com>
2026-06-09 11:04:38 +0200

    feat: search bar sur la liste

--- src/app/track-list/track-list.ts
+++ src/app/track-list/track-list.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, signal, computed } from '@angular/core';
 import { Track } from '../models/track';
 import { TrackCard } from '../track-card/track-card';
 
@@ -11,4 +11,18 @@ import { TrackCard } from '../track-card/track-card';
 export class TrackList {
   tracks = input.required<Track[]>();
   protected selectedId = signal<number | null>(null);
+
+  searchTerm = signal('');
+  filteredTracks = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+
+    if(!term){
+      return this.tracks();
+    }
+
+    return this.tracks().filter(track => 
+      track.title.toLowerCase().includes(term) ||
+      track.artist.toLowerCase().includes(term)
+    );
+  });
 }
